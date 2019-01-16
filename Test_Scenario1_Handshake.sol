24eed633ab9392570b8dfdd1c3700465e3239a0a
wstenzel <wills.stenzel@gmail.com>
2019-01-16 14:38:36 -0500

    renamed file

---
+++ Test_Scenario1_Handshake.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.25;
+
+import "./Scenario1_Handshake.sol";
+
+contract Test_Scenario1_Handshake is Scenario1_Handshake {
+
+  function test_PostTask() {
+    this.postTask.value(5000)(1);
+    assert(tasks[1].taskId == 1);
+    assert(tasks[1].owner == address(this));
+    assert(tasks[1].amount == 5000);
+    assert(tasks[1].isAwarded == false);
+    assert(tasks[1].isConfirmed == false);
+    assert(tasks[1].isPresent == true);
+  }
+
+  function test_PickTask() {
+    assert(tasks[1].evaluator == 0x0000000000000000000000000000000000000000);
+    pickTask(1);
+    assert(tasks[1].evaluator == msg.sender);
+  }
+
+  function test_ConfirmResult() {
+    assert(tasks[1].isConfirmed == false);
+    this.confirmResult(1);
+    assert(tasks[1].isConfirmed == true);
+  }
+
+  function test_AwardToken() {
+    assert(tasks[1].isAwarded == false);
+    awardToken(1);
+    assert(tasks[1].isAwarded == true);
+  }
+}
